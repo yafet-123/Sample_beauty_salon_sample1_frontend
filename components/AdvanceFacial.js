2ded05f287a580cb012fe003f3b4a16eae5e559b
yafet-123 <yafetaddisu123@gmail.com>
2022-10-19 14:06:40 +0300

    making responsive

--- components/AdvanceFacial.js
+++ components/AdvanceFacial.js
@@ -2,21 +2,21 @@ import React from "react";
 import Image from "next/image";
 export function AdvanceFacial() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 p-20">
-      <div className="flex flex-col just">
-        <h1 className="text-left text-black font-light text-4xl mb-5 capitalize">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:p-20">
+      <div className="flex flex-col">
+        <h1 className="text-left text-black font-light text-4xl mb-5 capitalize p-20 lg:p-20">
           Advance Facials
         </h1>
         <div className="bg-transparent lg:bg-indigo-900 w-full h-64 relative ">
-          <div className="advancefacial-background w-full lg:w-3/4 h-[40rem] mt-10 absolute top-0 bottom-0 left-0 right-0 m-auto"></div>
+          <div className="advancefacial-background w-full lg:w-3/4 h-[40rem] mt-10 absolute top-0 bottom-0 left-0 right-0 m-auto "></div>
         </div>
 
-        <p className="text-black font-normal text-xl mt-[30rem] capitalize text-center">
+        <p className="text-black font-normal text-xl mt-[30rem] capitalize text-center px-20 lg:px-20">
           Get in touch today to book and we will be happy to help
         </p>
       </div>
 
-      <div className="flex flex-col px-10">
+      <div className="flex flex-col lg:px-10">
         <Image
           src="/images/bgImage3.avif"
           width={500}
@@ -25,7 +25,7 @@ export function AdvanceFacial() {
           className=""
         />
 
-        <p className="text-center lg:text-left text-black font-normal text-xl my-10 capitalize">
+        <p className="text-center lg:text-left text-black font-normal text-xl my-10 capitalize p-20 lg:p-20">
           We advance facial offers the ultimate indulgence for ani-aging
           acne-control, skin calming and toning. Our advanced facials target
           problem areas to nourish regenerate and energise.we use botanical
